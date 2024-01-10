ee37977e184dc0f759f57b4fe8d3fecfb7659bd8
Isaac Lloyd <isaac@isaacclloyd.com>
2024-01-10 12:40:48 -0500

    update version

--- app/page.js
+++ app/page.js
@@ -3,11 +3,13 @@ import { Quiz } from '@/components/quiz';
 import { NameTag } from '@/components/nametag';
 import { Badge } from "@/components/ui/badge";
 
+// https://platform.openai.com/playground?assistant=asst_532NxdHgVLGprQJu4f8Td9Vd&thread=thread_E89QT6iaUgScmmA4QfI1iP8T
+
 export default function Home() {
   return (
     <>
       <div style={{ position: 'absolute', top: 5, right: 8 }}>
-        <Badge className="bg-slate-700">V4</Badge>
+        <Badge className="bg-slate-700">V5</Badge>
       </div>
       <NameTag />
       <Quiz />
