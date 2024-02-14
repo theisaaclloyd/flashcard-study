fcd4313616cfaea5275e97df8447ea41f8ee77af
Isaac Lloyd <57055268+theisaaclloyd@users.noreply.github.com>
2024-02-14 09:31:06 -0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,8 +3,6 @@ import { Quiz } from '@/components/quiz';
 import { NameTag } from '@/components/nametag';
 import { Badge } from "@/components/ui/badge";
 
-// https://platform.openai.com/playground?assistant=asst_532NxdHgVLGprQJu4f8Td9Vd&thread=thread_E89QT6iaUgScmmA4QfI1iP8T
-
 export default function Home() {
   return (
     <>
